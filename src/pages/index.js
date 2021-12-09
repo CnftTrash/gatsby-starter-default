60511123256edbe0875dc52a43bae5d7e16e4d42
CnftTrash <95837301+CnftTrash@users.noreply.github.com>
2021-12-09 07:40:00 -0800

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,22 +8,20 @@ import Seo from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <Seo title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
+    <h1>Welcome to CNFT Trash</h1>
+    <p>We take your unwanted NFTs and donate the TX fees to cleaning our Oceans!</p>
+    <p>We are just getting started. Donate and view the pool. </p>
     <StaticImage
-      src="../images/gatsby-astronaut.png"
+      src="../images/trash-lord.png"
       width={300}
       quality={95}
       formats={["auto", "webp", "avif"]}
-      alt="A Gatsby astronaut"
+      alt="Trash Lord"
       style={{ marginBottom: `1.45rem` }}
     />
     <p>
-      <Link to="/page-2/">Go to page 2</Link> <br />
-      <Link to="/using-typescript/">Go to "Using TypeScript"</Link> <br />
-      <Link to="/using-ssr">Go to "Using SSR"</Link> <br />
-      <Link to="/using-dsg">Go to "Using DSG"</Link>
+      <Link to="https://pool.pm/addr1qxw84x26sxg34u5x43ysxv7pqf20xc8l5p00rrv9p3c22xzqd3eqkvwnkrhvdvuw6y3mha9g4hh7gtjww38q8t25d5nqty6ulw">Trash Pool</Link> <br />
+     
     </p>
   </Layout>
 )
