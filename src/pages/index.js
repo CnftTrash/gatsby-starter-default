7adf03062e471eb11b598ecd148d03e5c4fcb0e3
CnftTrash <95837301+CnftTrash@users.noreply.github.com>
2021-12-09 08:24:16 -0800

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -11,6 +11,10 @@ const IndexPage = () => (
     <h1>Welcome to CNFT Trash</h1>
     <p>We take your unwanted NFTs and donate the TX fees to cleaning our Oceans!</p>
     <p>We are just getting started. Donate and view the pool. </p>
+  <p>
+      <Link to="https://pool.pm/addr1qxw84x26sxg34u5x43ysxv7pqf20xc8l5p00rrv9p3c22xzqd3eqkvwnkrhvdvuw6y3mha9g4hh7gtjww38q8t25d5nqty6ulw">Trash Pool</Link> <br />
+     
+    </p>
     <StaticImage
       src="../images/trash-lord.png"
       width={300}
@@ -19,10 +23,7 @@ const IndexPage = () => (
       alt="Trash Lord"
       style={{ marginBottom: `1.45rem` }}
     />
-    <p>
-      <Link to="https://pool.pm/addr1qxw84x26sxg34u5x43ysxv7pqf20xc8l5p00rrv9p3c22xzqd3eqkvwnkrhvdvuw6y3mha9g4hh7gtjww38q8t25d5nqty6ulw">Trash Pool</Link> <br />
-     
-    </p>
+    
   </Layout>
 )
 
