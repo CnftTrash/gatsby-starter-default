36787af2a64c0f3f338d36f07d50356f42aa72df
CnftTrash <95837301+CnftTrash@users.noreply.github.com>
2022-01-20 07:24:59 -0800

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,15 +10,15 @@ const IndexPage = () => (
     <Seo title="CNFT Trash" />
     <h1>Welcome to CNFT Trash</h1>
     <p>We take your unwanted NFTs and donate the TX fees to cleaning our Oceans!</p>
-    <p>We are just getting started. Donate Here: </p>
+    <p>We are just getting started. <strong> Donate Here: </strong> </p>
   <p></p>
   <p>
   addr1qxw84x26sxg34u5x43ysxv7pqf20xc8l5p00rrv9p3c22xzqd3eqkvwnkrhvdvuw6y3mha9g4hh7gtjww38q8t25d5nqty6ulw </p>
   <p></p>
-  or
+  or use
   <p></p>
   <p>
-   <strong> Our Donated $Handles </strong> | $51percent
+   <strong> Our Donated $handles </strong> | $51percent
 </p>
   <p>
       View the Pool <Link to="https://pool.pm/addr1qxw84x26sxg34u5x43ysxv7pqf20xc8l5p00rrv9p3c22xzqd3eqkvwnkrhvdvuw6y3mha9g4hh7gtjww38q8t25d5nqty6ulw">Trash Pool</Link> <br />
